949e1ba62f8a811b0cd399d3cf9ed6d061409545
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2019-08-28 18:24:30 +0800

    Update post-fs-data.sh

--- template_override/post-fs-data.sh
+++ template_override/post-fs-data.sh
@@ -3,3 +3,7 @@ MODDIR=${0%/*}
 
 # Reset context in case
 chcon -R u:object_r:system_file:s0 $MODDIR
+
+# Get ro.product.mod_device in build.prop
+CONFIG_PATH="/data/misc/riru/modules/miui_global_localization"
+sed -n '/ro.product.mod_device/{s/.*=//;p}' /system/build.prop > "$CONFIG_PATH/ro.product.mod_device"
