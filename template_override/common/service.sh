a346918ea6e820bd91a62f3718f32bff9c10d11c
Timothy Zhang <3147994+TimothyZhang023@users.noreply.github.com>
2019-11-06 11:43:15 +0800

    Update service.sh

--- template_override/common/service.sh
+++ template_override/common/service.sh
@@ -5,7 +5,3 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
-resetprop -n ro.miui.ui.version.name V9
-resetprop -n ro.miui.ui.version.code 7
-resetprop -n ro.miui.version.code_time 1505408400
-resetprop -n ro.miui.internal.storage /sdcard/
